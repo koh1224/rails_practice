90f9b46e3c23ed3e62393af2b22d21300aa4445f
EC2 Default User <ec2-user@ip-172-31-29-141.ec2.internal>
2021-09-09 08:18:34 +0000

    add 画像投稿機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -31,6 +31,7 @@ end
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
+
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3552b2cbc614ca56d2e2e8e130c94881b1387b25ab6a905ab9a107fe3a1a7bd583816cd3ae0be433fa8642b6e75a55f9f9d7fdaaa591e9d28623caf377ec0aa1'
